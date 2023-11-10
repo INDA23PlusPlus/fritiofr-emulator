32108568ffa2f5de6bb3985c5b4498b5216c187a
Fritiof Rusck <fritiof@rusck.se>
2023-11-10 11:25:52 +0100

    style: spacing

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -74,11 +74,11 @@ impl CPU {
         self.load_at_addr(PC_OFFSET, program);
         self.mem_write_u16(0xFFFC, PC_OFFSET);
     }
+
     pub fn load_at_addr(&mut self, addr: u16, program: Vec<u8>) {
         for (i, &byte) in program.iter().enumerate() {
             self.memory[(addr as usize) + i] = byte;
         }
-
     }
 
     /// When a cartridge was inserted into the NES, a reset signal was sent to the CPU. This
